4551c131d84ff8dbf5615bf72989739a112bc444
BuildTools <unconfigured@null.spigotmc.org>
2025-05-10 02:47:45 +0200

    initial commit

---
+++ soko_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SokoApp());
+}
+
+class SokoApp extends StatelessWidget {
+  const SokoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
